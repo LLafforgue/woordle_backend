f6ea5c60b484d7c71db9b080e6b4aa567f214c42
Ludovic.lafforgue.c@gmail.com <ludovic.lafforgue.c@gmail.com>
2025-07-02 15:05:38 +0200

    ajout route get

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('./models/connection')
 
 var app = express();
 var gameRouter = require('./routes/game');
+var indexRouter = require('./routes/index');
 
 const cors = require('cors');
 app.use(cors());
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/game', gameRouter);
 
 module.exports = app;
