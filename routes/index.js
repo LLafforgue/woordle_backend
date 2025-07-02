84b3d21bf10beb2ad0c10a6d0b9f61db1ef718ab
Ludovic.lafforgue.c@gmail.com <ludovic.lafforgue.c@gmail.com>
2025-07-02 14:58:28 +0200

    ajout route get

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+// un tableau de dix mots de votre choix que vous assignerez à la variable words.
+var express = require('express');
+var router = express.Router()
+const Game = require("../models/games")
+
+let words = [
+'pyrope', 'vecteur', 'roses', 'projet', 'nsutite', 'soupe', 'brocoli', 'biere', 'ragot', 'chat', 'design', 'libellule'
+]
+
+router.get('/', (req, res)=>{
+    let index = Math.floor(Math.random()*(words.length-1))
+    let word = words[index];
+    res.json({result: true,
+             word
+    })
+})
+
+module.exports = router
